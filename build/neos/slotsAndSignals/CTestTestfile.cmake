b6dfc9f8986a4c14c28c0fcdd5269371bc3f16d5
nboe <n.boeninger@web.de>
2024-02-25 11:56:25 +0100

    init cmake struct and stuff...

---
+++ build/neos/slotsAndSignals/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nboe/projects/Neos/neos/slotsAndSignals
+# Build directory: /home/nboe/projects/Neos/build/neos/slotsAndSignals
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
---
+++ build/neos/slotsAndSignals/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nboe/projects/Neos/neos/slotsAndSignals/test
+# Build directory: /home/nboe/projects/Neos/build/neos/slotsAndSignals/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SignalSlotTests "/home/nboe/projects/Neos/build/neos/slotsAndSignals/test/SignalSlotTests")
+set_tests_properties(SignalSlotTests PROPERTIES  _BACKTRACE_TRIPLES "/home/nboe/projects/Neos/neos/slotsAndSignals/test/CMakeLists.txt;16;add_test;/home/nboe/projects/Neos/neos/slotsAndSignals/test/CMakeLists.txt;0;")
